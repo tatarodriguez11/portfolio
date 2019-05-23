bd59a763ce64e9189200361356673ce6c9e633af
tatarodriguez11 <rodriguezjessicatatiana@gmail.com>
2019-05-22 20:57:34 -0500

    skills and education logos added

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,10 +7,7 @@ class About extends Component {
       var name = this.props.data.name;
       var profilepic = "images/" + this.props.data.image;
       var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
+
       var phone = this.props.data.phone;
       var email = this.props.data.email;
       var resumeDownload = this.props.data.resumedownload;
@@ -31,16 +28,14 @@ class About extends Component {
                 <h2>Contact Details</h2>
                 <p className="address">
                   <span>{name}</span><br />
-                  <span>{street}<br />
-                    {city} {state}, {zip}
-                  </span><br />
+
                   <span>{phone}</span><br />
                   <span>{email}</span>
                 </p>
               </div>
               <div className="columns download">
                 <p>
-                  <a href={resumeDownload} target="_blank" className="button"><i className="fa fa-download"></i>Download Resume</a>
+                  <a href={resumeDownload} target="_blank" className="button"><i className="fa fa-download"></i>Download Resume (Spanish)</a>
                 </p>
               </div>
             </div>
